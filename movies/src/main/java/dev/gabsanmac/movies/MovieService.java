716b0b893544503abd9ff35fdf0cf492d49f159d
GabrielSanMac <gabrielsantosmachado.sm@gmail.com>
2023-10-13 20:32:54 -0300

    Only One Commit

---
+++ movies/src/main/java/dev/gabsanmac/movies/MovieService.java
@@ -0,0 +1,22 @@
+package dev.gabsanmac.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+    @Autowired
+    private MovieRepository repository;
+    public List<Movie> findAllMovies() {
+        return repository.findAll();
+    }
+
+    public Optional<Movie> findSingleMovie(String imdbId) {
+        return repository.findMovieByImdbId(imdbId);
+    }
+
+}
